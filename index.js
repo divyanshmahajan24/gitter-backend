6881af108a8f44389299e024971acaad5e9c797a
Divyansh Mahajan <divyanshmahajan.24@gmail.com>
2018-04-10 23:18:53 +0530

    Mongo client setup

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const app = express();
+const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db');
 
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
+const app = express();
 
-const server = app.listen(8080, function () {
-   const host = server.address().address
-   const port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err);
+  require('./routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
+});

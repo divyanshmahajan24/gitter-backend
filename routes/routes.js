0bde1e9c80c2b56a692575697c84614e064a0e3e
Divyansh Mahajan <divyanshmahajan.24@gmail.com>
2018-04-11 00:06:37 +0530

    Feature: Add GET, PUT, POST, DELETE routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,51 @@
-module.exports = function(app, db) {
+const ObjectID = require('mongodb').ObjectID;
+
+module.exports = function (app, db) {
   app.post('/notes', (req, res) => {
-    // You'll create your note here.
-    console.log(req.body);
-    res.send('Hello');
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) {
+        res.send({ error: 'An error has occurred' });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+
+  app.get('/notes/:id', (req, res) => {
+    const { id } = ({ id: req.params.id });
+    const details = { _id: new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({ error: 'An error has occurred' });
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
+  app.delete('/notes/:id', (req, res) => {
+    const { id } = ({ id: req.params.id });
+    const details = { _id: new ObjectID(id) };
+    db.collection('notes').remove(details, (err) => {
+      if (err) {
+        res.send({ error: 'An error has occurred' });
+      } else {
+        res.send(['Note ', id, ' deleted!'].join());
+      }
+    });
+  });
+
+  app.put('/notes/:id', (req, res) => {
+    const { id } = ({ id: req.params.id });
+    const details = { _id: new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').update(details, note, (err) => {
+      if (err) {
+        res.send({ error: 'An error has occurred' });
+      } else {
+        res.send(note);
+      }
+    });
   });
 };
